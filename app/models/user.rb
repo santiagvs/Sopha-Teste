8d59c52bbb4fc9dde5ddbe0c8059f272bfd9e634
Santiago <victor.lacsilva0102@gmail.com>
2023-09-18 16:37:39 -0300

    removendo tabela Users

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
-
-  has_many :stores, dependent: :destroy
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230917125335) do
+ActiveRecord::Schema.define(version: 20230918193641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,14 +18,6 @@ ActiveRecord::Schema.define(version: 20230917125335) do
   create_table "stores", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
